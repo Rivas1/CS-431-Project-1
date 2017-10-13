a5cfa31732e00bfaf914d9f1eedc6b888383c378
Rivas1 <rivas1@cpp.edu>
2017-10-13 13:04:57 -0700

    First Come First Serve Scheduler Complete
    As long as the text file is in the same directory as the program, the program will run.

--- fcfs.java
+++ fcfs.java
@@ -10,12 +10,14 @@ public class fcfs
 		// pid
 		// burst_time
 		// priority
+		
 		int[] processes = new int[90];
 		String fileName = "testdata1.txt";
 		
 		processes = read_in_file(fileName, processes);
 
-		print_processes(processes);
+		// print_processes(processes);
+		run_scheduler( processes );
 	}
 
 	public static int[] read_in_file ( String fileName, int[] processes )
@@ -28,7 +30,7 @@ public class fcfs
 		{
 			FileReader fileReader = new FileReader(fileName);
 			BufferedReader bufferedReader = new BufferedReader(fileReader);
-
+			System.out.println("Test file: " + fileName);
 			while ( (line = bufferedReader.readLine()) != null )
 			{
 				// read in pid
@@ -81,4 +83,24 @@ public class fcfs
 		}
 		
 	}
+
+	public static void run_scheduler ( int[] processes )
+	{
+		long current = 0;
+		long elapsedTime = 0;
+		System.out.println("Initial time: " + current + "\n" );
+
+		for (int i = 0; i < numOfProcesses * 3; i = i + 3)
+		{
+			long a = System.currentTimeMillis();
+			System.out.print("Running process: " + processes[i] 
+							+ " | elapsed time: " );
+			long b = System.currentTimeMillis();
+			long c = b - a;
+			elapsedTime = elapsedTime + c + processes[i + 1];
+			System.out.println( elapsedTime ); // adding elapsed time + burst time
+		}
+		System.out.println("\nFinal time: " + elapsedTime);
+	}
+
 }
