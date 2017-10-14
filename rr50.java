93b140c93d2e57965d3efad1eee41e56bff723bd
Rivas1 <rivas1@cpp.edu>
2017-10-13 21:05:16 -0700

    Add files via upload

---
+++ rr50.java
@@ -0,0 +1,81 @@
+import java.io.*;
+
+public class rr50
+{
+	static int numOfProcesses = 0;
+	public static void main ( String[] args ) throws IOException
+	{
+		process[] processList = new process[30];
+
+		String fileName = "testdata1.txt"; // change this when testing other test data files
+		
+		processList = read_in_file(fileName, processList);
+		run_scheduler( processList );
+	}
+
+	public static process[] read_in_file ( String fileName, process[] processes ) throws IOException
+	{
+		String line = null; 
+		int i = 0, j = 0;
+		int pid = 0, bt = 0, priority = 0;
+
+		try
+		{
+			FileReader fileReader = new FileReader(fileName);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			System.out.println("Test file: " + fileName);
+			while ( (line = bufferedReader.readLine()) != null )
+			{
+				
+				// read in pid
+				if ( i % 3 == 0 ) { pid =  Integer.parseInt(line); i++; continue; }
+				
+				if ( i % 3 == 1 ) { bt = Integer.parseInt(line); i++; continue; }
+				
+				if ( i % 3 == 2 ) { priority = Integer.parseInt(line); i++; }
+
+				if ( i % 3 == 0)
+				{
+					process a = new process (pid, bt, priority);
+					processes[j] = a;
+					// System.out.println("PID: " + processes[j].getPID() );
+					// System.out.println("BT: " + processes[j].getBurstTime() );
+					// System.out.println("Priority: " + processes[j].getPriority() );
+					j++;
+				}
+			}
+			numOfProcesses = i / 3;
+			System.out.println("Read in " + numOfProcesses + " processes.");
+
+			bufferedReader.close();
+		}
+		catch ( FileNotFoundException e )
+		{
+			System.out.println("Could not locate " + fileName);
+		}
+		catch ( IOException e )
+		{
+			System.out.println("Error reading " + fileName);
+		}
+
+		return processes;
+	}
+	public static void run_scheduler ( process[] processes )
+	{
+		long current = 0;
+		long elapsedTime = 0;
+		System.out.println("Initial time: " + current + "\n" );
+
+		for (int i = 0; i < numOfProcesses; i++)
+		{
+			long a = System.currentTimeMillis();
+			System.out.print("Running process: " + processes[i].getPID() 
+							+ " | elapsed time: " );
+			long b = System.currentTimeMillis();
+			long c = b - a;
+			elapsedTime = elapsedTime + c + processes[i].getBurstTime() + 50 + 3;  // adding elapsed time + burst time + round robin
+			System.out.println( elapsedTime );
+		}
+		System.out.println("\nFinal time: " + elapsedTime);
+	}
+}
